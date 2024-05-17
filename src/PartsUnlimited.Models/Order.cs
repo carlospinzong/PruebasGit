01db956c3c80468f25ea64564c70350cda8f6cc5
carlospinzong <carpin43@gmail.com>
2024-05-17 13:40:58 -0500

    Cambio 2

--- src/PartsUnlimited.Models/Order.cs
+++ src/PartsUnlimited.Models/Order.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// Actualizacion 17/05/2023
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
